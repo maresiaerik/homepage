2cad410432899ab4b36b6b91bc042c670dbed80a
Erik Maresia <maresiaerik@protonmail.com>
2023-09-02 19:08:03 +0200

    Add no ssr wrapper

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,21 @@
+"use-client";
+
 import { ReactElement } from "react";
 
 import RootLayoutWrapper from "@/layouts/RootLayoutWrapper";
 
+import NonSSRWrapper from "@/components/NonSSRWrapper";
 import ParentProvider from "./providers";
 
 export default function RootLayout({ children }: { children: React.ReactNode }): ReactElement {
   return (
     <html lang="en">
       <body>
-        <ParentProvider>
-          <RootLayoutWrapper>{children}</RootLayoutWrapper>
-        </ParentProvider>
+        <NonSSRWrapper>
+          <ParentProvider>
+            <RootLayoutWrapper>{children}</RootLayoutWrapper>
+          </ParentProvider>
+        </NonSSRWrapper>
       </body>
     </html>
   );
